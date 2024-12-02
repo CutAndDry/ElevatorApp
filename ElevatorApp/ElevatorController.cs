79d9552aa739470b3a65770fc955376941d050bf
marku <marku@DESKTOP-UKV4JGU>
2024-12-02 22:45:43 +0200

    Created unit tests and basic class structure to start the project

---
+++ ElevatorApp/ElevatorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorApp
+{
+    public class ElevatorController
+    {
+        public List<Elevator> elevators;
+        public Dictionary<int, Queue<ElevatorRequest>> floorRequests;
+
+        public void AddElevator(Elevator elevator)
+        {
+        
+        }
+        public void RequestElevator(int floor, int destination, int passengerCount)
+        {
+
+        }
+
+        public async Task HandleRequestsAsync()
+        {
+        }
+
+    }
+}
