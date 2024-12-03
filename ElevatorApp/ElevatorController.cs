2caf7daed4fd5338a9255b9c013017b7c0c245fa
marku <marku@DESKTOP-UKV4JGU>
2024-12-03 16:33:01 +0200

    Created a way to add elevators and queue up elevator Requests

--- ElevatorApp/ElevatorController.cs
+++ ElevatorApp/ElevatorController.cs
@@ -11,12 +11,28 @@ namespace ElevatorApp
         public List<Elevator> elevators;
         public Dictionary<int, Queue<ElevatorRequest>> floorRequests;
 
+        public ElevatorController()
+        {
+            elevators = new List<Elevator>();
+            floorRequests = new Dictionary<int, Queue<ElevatorRequest>>();
+        }
+
+
         public void AddElevator(Elevator elevator)
         {
-        
+            elevators.Add(elevator);
+            floorRequests[elevator.CurrentFloor] = new Queue<ElevatorRequest>();
         }
         public void RequestElevator(int floor, int destination, int passengerCount)
         {
+            // Add request to the request queue
+            if (!floorRequests.ContainsKey(floor))
+            {
+                floorRequests[floor] = new Queue<ElevatorRequest>();
+            }
+
+            var elevatorRequest = new ElevatorRequest(destination, passengerCount);
+            floorRequests[floor].Enqueue(elevatorRequest);
 
         }
 
